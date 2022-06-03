79e0bc4fd9c2e928a65e1a0c29ca2e04fbca3608
Alessandro Marotta <alessandro.marotta@samag.tech>
2022-06-03 12:54:26 +0200

    Aggiunto exception per la gestione delle eccezioni del core

---
+++ src/Exceptions/CoreException.php
@@ -0,0 +1,12 @@
+<?php namespace SamagTech\CoreLumen\Exceptions;
+
+use Exception;
+
+/**
+ * Eccezione per la segnalazione degli errori
+ * nell'utilizzo del core
+ *
+ * @author Alessandro Marotta <alessandro.marotta@samag.tech>
+ * @since v0.1
+ */
+class CoreException extends Exception {}
