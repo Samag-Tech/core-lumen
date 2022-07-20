0e0dd97cf7893730f805dae515addbf1cc268c1b
Alessandro Marotta <alessandro.marotta@samag.tech>
2022-07-20 12:53:26 +0200

    Aggiunto comando per la generazione di una nuova chiave di servizio

---
+++ src/Console/AddServiceKeyCommand.php
@@ -0,0 +1,42 @@
+<?php namespace SamagTech\CoreLumen\Console;
+
+use Illuminate\Console\Command;
+use SamagTech\CoreLumen\Models\ServiceKey;
+
+/**
+ * Comando per la generazione di una nuova chiave all'interno
+ * della tabella delle chiavi dei servizi
+ *
+ * @author Alessandro Marotta <alessandro.marotta@samag.tech>
+ */
+class AddServiceKeyCommand extends Command {
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'core:add-service-key {suffix}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Aggiunge una nuova chiave per un servizio';
+
+    /**
+     * Execute the console command.
+     *
+     * @param  \App\Support\DripEmailer  $drip
+     * @return mixed
+     */
+    public function handle(ServiceKey $serviceKey) {
+
+        $suffix = $this->argument('suffix');
+
+        $key = $serviceKey->create(['suffix' => $suffix]);
+
+        $this->info("Chiave per il suffisso $suffix : $key->id");
+    }
+}
--- src/Providers/GeneratorProvider.php
+++ src/Providers/GeneratorProvider.php
@@ -1,5 +1,6 @@
 <?php namespace SamagTech\CoreLumen\Providers;
 
+use SamagTech\CoreLumen\Console\AddServiceKeyCommand;
 use SamagTech\CoreLumen\Console\ServiceKeyTableCommand;
 
 /**
@@ -12,7 +13,8 @@ use SamagTech\CoreLumen\Console\ServiceKeyTableCommand;
 class GeneratorProvider extends BaseGeneratorProvider {
 
     protected array $commands = [
-        'ServiceKeyTable'   => 'servicekeytable'
+        'ServiceKeyTable'   => 'servicekeytable',
+        'AddServiceKey'     => 'addservicekey'
     ];
 
     //-----------------------------------------------------------------------
@@ -27,6 +29,22 @@ class GeneratorProvider extends BaseGeneratorProvider {
 
         app()->singleton($this->getPrefixBinding().'servicekeytable', function ($app) {
             return new ServiceKeyTableCommand($app['files']);
+        });
+
+    }
+
+    //-----------------------------------------------------------------------
+
+    /**
+     * Registra il comando per la creazione di una nuova chiave della tabella
+     * 'services_keys'
+     *
+     * @return void
+     */
+    public function registerAddServiceKeyCommand() : void {
+
+        app()->singleton($this->getPrefixBinding().'addservicekey', function ($app) {
+            return new AddServiceKeyCommand();
         });
 
     }
