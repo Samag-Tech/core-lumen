f2f816f2df229dac18ed594b5f7cd6ac486f8e75
Alessandro Marotta <alessandro.marotta@samag.tech>
2022-07-20 12:36:00 +0200

    Aggiunto modello per la gestione delle chiavi dei servizi

---
+++ src/Models/ServiceKey.php
@@ -0,0 +1,28 @@
+<?php namespace SamagTech\CoreLumen\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Modello per la gestione delle chiavi dei servizi
+ *
+ * @extends Model
+ *
+ * @author Alessandro Marotta <alessandro.marotta@samag.tech>
+ */
+class ServiceKey extends Model {
+
+    protected $fillable = [
+        'id',
+        'suffix'
+    ];
+
+    protected $table = 'services_keys';
+
+    protected $primaryKey = 'id';
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
+    public $timestamps = false;
+}
