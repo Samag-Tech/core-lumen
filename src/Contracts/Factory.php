cdb131366282399979a1c7e58e5347c521e59245
Alessandro Marotta <alessandro.marotta@samag.tech>
2022-07-20 12:33:15 +0200

    Modifica interfaccia factory

--- src/Contracts/Factory.php
+++ src/Contracts/Factory.php
@@ -1,6 +1,7 @@
 <?php namespace SamagTech\CoreLumen\Contracts;
 
 use SamagTech\CoreLumen\Contracts\Service;
+use SamagTech\CoreLumen\Models\ServiceKey;
 
 /**
  * Definizione di un interfaccia per l'applicazione
@@ -18,9 +19,10 @@ interface Factory {
      * Funzione che restituisce il servizio da chiamare
      * in base ad un token.
      *
+     * @param ServiceKey    $serviceKey Modello per la gestione delle chiavi
      * @param string $token Token che identifica quale servizio istanziare
      *
      * @return \SamagTech\CoreLumen\Contracts\Service;
      */
-    public function makeService( ?string $token = null ) : Service;
+    public function makeService(ServiceKey $serviceKey, ?string $token = null ) : Service;
 }
